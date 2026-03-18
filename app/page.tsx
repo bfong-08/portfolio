f3ac240b78d4f2f8a8a6fd4826722635c8f64a57
bfong-08 <branfong21@gmail.com>
2026-03-18 09:27:42 -0700

    add featured project to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Home() {
   return (
@@ -11,11 +12,35 @@ export default function Home() {
         <div className="flex flex-col gap-12 w-sm">
           <h1 className="text-7xl">Brandon Fong</h1>
           <p>
-            Hello! I am a high school senior at St. Joseph Notre Dame High School. My passion lies in mathematics and quantum physics, and I intend to pursue a degree in physics.
+            Hello! I am a high school senior at St. Joseph Notre Dame High School 
+            who has a passion mathematics and quantum physics. 
+            I'm not committed to college yet, but I intend to pursue a degree in physics!
           </p>
         </div>
       </header>
-      <div className="w-full h-96 bg-gunmetal flex items-center justify-center">
+      <div className="w-full pt-12 pb-18 gap-6 bg-linear-to-tl from-salmon to-pink text-gunmetal flex flex-col items-center justify-center">
+        <h1>
+          Featured Projects
+        </h1>
+        <Link href="https://github.com/bfong-08/quantum-simulator" 
+        className="h-48 w-4xl overflow-hidden rounded-xl flex item-center justify-center 
+        relative group">
+          <Image src="/quantum-simulator-visual.jpeg" alt="quantum simulator visual" 
+          width={1000} height={1000} className="w-full object-cover group-hover:scale-110 group-hover:brightness-50
+          transition-all duration-200"/>
+          <div className="flex flex-col justify-end absolute bottom-0 left-0 w-full h-full p-4 z-10 
+          text-gray bg-linear-to-t from-black to-black/">
+            <h2 className="font-bold text-2xl">
+              Quantum State Simulator
+            </h2>
+            <p>
+              A lightweight simulator created using Python and NumPy
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div className="w-full py-12 gap-8 bg-gunmetal text-gray flex flex-col items-center justify-center">
+
       </div>
     </div>
   );
